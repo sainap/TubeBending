4956078e55acc2bb641a51d154d88285a28ddc4a
sainap <parsis@uw.edu>
2024-10-29 16:05:01 -0700

    Update 3d-Lights.js

--- 3d-Lights.js
+++ 3d-Lights.js
@@ -23,25 +23,30 @@ export class LightSetup {
         this.scene = scene;
         this.lights = [];
         this.createLights();
+
+        this.scene.add(...this.lights)
+
     }
 
     createLights() {
         this.ambientLight = new THREE.AmbientLight(this.config.lights.ambientLight.color, this.config.lights.ambientLight.intensity);
         this.scene.add(this.ambientLight);
-        this.lights.push(this.ambientLight); 
+        this.lights.push(this.ambientLight);
 
         this.pointLights = this.config.lights.pointLight.positions.map(position => {
             const pointLight = this.createPointLight(position);
             this.lights.push(pointLight);
             return pointLight;
         });
+
+        // Light that moves dynamically, shining on the bend center
+        this.light = this.pointLights[0];
     }
 
     createPointLight(position) {
         const pointLight = new THREE.PointLight(this.config.lights.pointLight.color, this.config.lights.pointLight.intensity);
         pointLight.castShadow = this.config.lights.pointLight.castShadow;
         pointLight.position.set(position.x, position.y, position.z);
-        this.scene.add(pointLight);
         return pointLight;
     }
 
